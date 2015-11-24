1020c623128e6f262412130f21a5dbcc0ce31246
Roberto Despoiu <despoiu.kilo@gmail.com>
2015-11-23 20:09:09 -0600

    Initial Commit

---
+++ War-CardGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  War-CardGame
+//
+//  Created by Roberto Despoiu on 11/23/15.
+//  Copyright © 2015 Roberto Despoiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
